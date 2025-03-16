b86a67d953133c981f4bf802d5c2e697f9b5740b
Abhijeet <abhijeetanand407@gmail.com>
2025-03-16 15:18:22 +0530

    Basic Navigation Bar

---
+++ client/src/component/Navbar.jsx
@@ -0,0 +1,31 @@
+import styles from './Navbar.module.css'
+import { CiSearch } from "react-icons/ci";
+import { MdNotificationsNone } from "react-icons/md";
+import { FaUserCircle } from "react-icons/fa";
+
+export default function Navbar() {
+  return (
+    <div className={styles.container}>
+        <div className={styles.sync}>
+       <div className={styles.title}>StreamSync</div>
+       <div>
+        <ul className={styles.categories}>
+            <li>Home</li>
+            <li>Browse</li>
+            <li>Following</li>
+            <li>Categories</li>
+        </ul>
+       </div>
+       </div>
+
+       <div className={styles.options}>
+        <input type="text" />
+       
+       <CiSearch size={20}/>
+       <MdNotificationsNone size={20}/>
+       <FaUserCircle size={22} />
+       </div>
+
+    </div>
+  )
+}
